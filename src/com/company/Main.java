176e9ea858207f49da0f968ef45b4bfbfea3eeef
Alex Beley <sasha.school.2005@gmail.com>
2020-06-18 17:43:55 +0300

    1-st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1)Підйом");
+        System.out.println("2)Сніданок і душ");
+        System.out.println("3)1.5 год зал");
+        System.out.println("4)Обід");
+        System.out.println("5)2 год англійської");
+        System.out.println("6)2 год програмування");
+        System.out.println("7)1.5 год математики");
+        System.out.println("Відпочинок");
+
+
+
+
+
+
+    }
+}
